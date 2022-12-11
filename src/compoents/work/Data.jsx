63ea0243dde61255567afe51966cb9c150296731
joycehwchan <joycehwchan@gmail.com>
2022-12-11 19:06:52 +0900

    added new project

--- src/compoents/work/Data.jsx
+++ src/compoents/work/Data.jsx
@@ -1,45 +1,58 @@
-import Work1 from "../../assets/work1.jpg"
-import Work2 from "../../assets/work2.jpg"
-import Work3 from "../../assets/work3.jpg"
+import Work1 from "../../assets/work1.jpg";
+import Work2 from "../../assets/work2.jpg";
+import Work3 from "../../assets/work3.jpg";
+import Work4 from "../../assets/work4.jpg";
 
 export const projectsData = [
-    {
-        id: 1,
-        image: Work1,
-        title: '旅Now',
-        description: '旅行のしおりを自動生成、自由にカスタマイズ。',
-        link: 'https://tabinow.tours/',
-        github: 'https://github.com/joycehwchan/tabinow',
-        category: 'web',
-        role: 'Front-end Lead',
-        used: 'HTML/Sass, Javascript, Ruby on Rails, Devise, Pundit, APIs, Cloudinary, Mapbox, Figma'
-    },
-    {
-        id: 2,
-        image: Work2,
-        title: 'MinaNoka',
-        description: '近くの畑を借りて野菜作り、みんなで農家。',
-        link: '',
-        github: 'https://github.com/joycehwchan/minanoka',
-        category: 'web',
-        role: 'Lead Developer',
-        used: 'HTML/CSS, Bootstrap, Ruby on Rails, Devise, Cloudinary, Mapbox, Figma'
-    },
-    {
-        id: 3,
-        image: Work3,
-        title: 'ポートフォリオ',
-        description: 'プロジェクトを載せるための個人サイト。',
-        link: '#',
-        github: 'https://github.com/joycehwchan/portfolio',
-        category: 'design',
-        role: 'Front-end Developer, UI Designer',
-        used: 'Figma, React, CSS'
-    },
-]
+  {
+    id: 1,
+    image: Work1,
+    title: "旅Now",
+    description: "旅行のしおりを自動生成、自由にカスタマイズ。",
+    link: "https://tabinow.tours/",
+    github: "https://github.com/joycehwchan/tabinow",
+    category: "web",
+    role: "Front-end Lead",
+    used: "HTML/Sass, Javascript, Ruby on Rails, Devise, Pundit, APIs, Cloudinary, Mapbox, Figma",
+  },
+  {
+    id: 2,
+    image: Work2,
+    title: "MinaNoka",
+    description: "近くの畑を借りて野菜作り、みんなで農家。",
+    link: "",
+    github: "https://github.com/joycehwchan/minanoka",
+    category: "web",
+    role: "Lead Developer",
+    used: "HTML/CSS, Bootstrap, Ruby on Rails, Devise, Cloudinary, Mapbox, Figma",
+  },
+  {
+    id: 3,
+    image: Work3,
+    title: "ポートフォリオ",
+    description: "プロジェクトを載せるための個人サイト。",
+    link: "#",
+    github: "https://github.com/joycehwchan/portfolio",
+    category: "design",
+    role: "Front-end Developer, UI Designer",
+    used: "Figma, React, CSS",
+  },
+  {
+    id: 4,
+    image: Work4,
+    title: "記憶ゲーム",
+    description: "同じキャラを2回クリックしたら負け！",
+    link: "https://joycehwchan.github.io/memory-game/",
+    github: "https://github.com/joycehwchan/memory-game",
+    category: "fun",
+    role: "Front-end Developer, UI Designer",
+    used: "React, CSS",
+  },
+];
 
 export const projectsNav = [
-    {name: 'all'},
-    {name: 'web'},
-    {name: 'design'},
-]
+  { name: "all" },
+  { name: "web" },
+  { name: "design" },
+  { name: "fun" },
+];
